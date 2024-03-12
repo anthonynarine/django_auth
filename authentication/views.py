b352aa38514a3ad0c42b40e6cd6b0d6d716dc063
anthonynarine <anarine83@gmail.com>
2024-03-11 22:27:50 -0400

    reverting back to api docs

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+# def home(request):
+#     return HttpResponse("SURPRISE MOTHER FUCKER")
 
 def home(request):
-    return HttpResponse("SURPRISE MOTHER FUCKER")
+    return HttpResponseRedirect('https://documenter.getpostman.com/view/23868442/2sA2xh3tTu')
+
+
+
