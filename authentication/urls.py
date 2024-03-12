4907721a82893741675152435853f9ea4add08c5
anthonynarine <anarine83@gmail.com>
2024-03-11 21:54:46 -0400

    Add root URL configuration

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,13 @@
 
+from os import name
+from turtle import home
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("user.urls") )
+    path("api/", include("user.urls")),
+    path('', views.home, name='home'),
+    
 ]
