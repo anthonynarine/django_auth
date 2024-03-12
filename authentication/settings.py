41d25d5a8e669f79152f7c8ce6e8058aa0c23469
anthonynarine <anarine83@gmail.com>
2024-03-11 21:45:03 -0400

    Fix Heroku deployment issues

--- authentication/settings.py
+++ authentication/settings.py
@@ -14,7 +14,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['ant-django-auth-62cf01255868.herokuapp.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
