6753e5dddc2ff26f5fce0f9f236c109fbdb9ab32
anthonynarine <anarine83@gmail.com>
2024-03-11 22:22:01 -0400

    renamed view from redirect to home

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponseRedirect
 
-def redirect_to_docs(request):
+def home(request):
     return HttpResponseRedirect('https://documenter.getpostman.com/view/23868442/2sA2xh3tTu')
