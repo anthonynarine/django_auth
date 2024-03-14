05b50655522774dad165f0149fa730e72fcd5321
anthonynarine <anarine83@gmail.com>
2024-03-13 22:39:20 -0400

    updated home page

--- authentication/views.py
+++ authentication/views.py
@@ -6,11 +6,11 @@ from django.shortcuts import render
 # def home(request):
 #     return HttpResponse("SURPRISE MOTHER FUCKER")
 
-# def home(request):
-#     return HttpResponseRedirect('https://documenter.getpostman.com/view/23868442/2sA2xh3tTu')
-
-
 def home(request):
-    return render(request, "home.html")
+    return HttpResponseRedirect('https://documenter.getpostman.com/view/23868442/2sA2xh3tTu')
+
+
+# def home(request):
+#     return render(request, "home.html")
 
 
