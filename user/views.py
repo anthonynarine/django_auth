a8bdbc24f19ae3d96f18bd4ea19ee0eb5a77a18f
anthonynarine <anarine83@gmail.com>
2024-05-20 21:47:06 -0400

    moving to mac

--- user/views.py
+++ user/views.py
@@ -57,11 +57,11 @@ print(User)
 
 logger = logging.getLogger(__name__)
 # Test logging
-logger.debug("This is a debug message")
-logger.info("This is an info message")
-logger.warning("This is a warning message")
-logger.error("This is an error message")
-logger.critical("This is a critical message")
+# logger.debug("This is a debug message")
+# logger.info("This is an info message")
+# logger.warning("This is a warning message")
+# logger.error("This is an error message")
+# logger.critical("This is a critical message")
 
 # ANSI color codes for logger
 RED = '\033[91m'
