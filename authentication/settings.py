3333a5601e6591cdb8c6cf829da430c5332b01b9
anthonynarine <anarine83@gmail.com>
2024-06-25 21:22:42 -0400

    Add Netlify app to CSRF_TRUSTED_ORIGINS and CORS_ALLOWED_ORIGINS

--- authentication/settings.py
+++ authentication/settings.py
@@ -133,6 +133,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
     "https://ant-django-auth-62cf01255868.herokuapp.com",
+    'https://gait.netlify.app',
     # Additional origins...
 ]
 CORS_ALLOW_CREDENTIALS = True
@@ -141,6 +142,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:3001',
     "https://ant-django-auth-62cf01255868.herokuapp.com",
+    'https://gait.netlify.app',
     # Additional trusted origins...
 ]
 
